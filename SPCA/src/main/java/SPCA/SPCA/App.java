1e285ef911565940a9ddd48aaa2be4a01aee81ab
FinalYearProjectWW <c21467734@mytudublin.ie>
2025-03-10 16:08:58 +0000

    First Commit.

---
+++ SPCA/src/main/java/SPCA/SPCA/App.java
@@ -0,0 +1,13 @@
+package SPCA.SPCA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
